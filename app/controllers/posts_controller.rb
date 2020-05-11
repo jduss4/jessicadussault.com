28d01d647a15951301df032a0d5f629d7f99f005
Jessica Dussault <jdussault4@gmail.com>
2020-05-10 21:20:09 -0500

    starting to fill in pages and hammer down styles
    adds fancy CSS for navbar
    copies in Kodaly post with looots of brs
    because I decided I would rather have those than try
    to style some one-off ul thing

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,6 @@
+class PostsController < ApplicationController
+
+# TODO show action
+# should throw 404 if there is no id matching a template
+
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
 
+  def nav_link_class(link)
+    if current_page? link
+      "nav-link active"
+    else
+      "nav-link"
+    end
+  end
+
   def title
     [ @title, "J Dussault" ].compact.join(" | ")
   end
