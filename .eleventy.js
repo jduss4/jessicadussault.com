48542f9ce75d29f99ec468d6ce61aa3daf0eb20e
Jessica Dussault <jdussault4@gmail.com>
2022-11-09 08:05:48 -0600

    adds cookblub page and data, starts sidequest page

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const yaml = require("js-yaml");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.setBrowserSyncConfig({
     files: './_site/assets/css/**/*.css'
@@ -5,6 +7,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy(
     { "_assets/images/": "assets/images" }
   );
+  eleventyConfig.addDataExtension("yml", contents =>
+    yaml.load(contents)
+  )
   return {
     dir: {
       includes: "_includes",
---
+++ sidequests/index.md
@@ -0,0 +1,8 @@
+---
+title: Side Quests
+layout: default.njk
+---
+
+Things I want to put on a website that are not blogs, I guess.
+
+[Cook Blub book list](cookblub)
